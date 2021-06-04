d19dad8e5c2e31e8a27cc26d0a3f12a97faf3e9c
Sowbharnika <sowbharnika.karthikeyan@banfico.com>
2021-06-04 19:52:01 +0530

    DAO design https://github.com/sowbharni-05/E-ComRest-Impl/issues/6#issue-911158628

---
+++ src/main/java/com/banfico/EcomApplication/Dao/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.banfico.EcomApplication.Dao;
+
+import com.banfico.EcomApplication.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+}
