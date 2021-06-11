801c32d8815628488f577d6c98403763ca2b3f1a
Sowbharnika <sowbharnika.karthikeyan@banfico.com>
2021-06-12 00:14:14 +0530

    Revert "Added Custom Query Methods-Entity Design https://github.com/sowbharni-05/E-ComRest-Impl/issues/3#issue-910471175"
    This reverts commit cf75d56f

--- src/main/java/com/banfico/EcomApplication/Dao/OrderRepo.java
+++ src/main/java/com/banfico/EcomApplication/Dao/OrderRepo.java
@@ -4,11 +4,6 @@ import com.banfico.EcomApplication.Model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OrderRepo extends JpaRepository<OrderDetail,Integer> {
-
-    List<OrderDetail> findByOrderByOrderDateAsc();
-
 }
