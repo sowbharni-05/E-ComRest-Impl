cf75d56f09683edcc2a6f23fbaec6e48d2a4e91c
Sowbharnika <sowbharnika.karthikeyan@banfico.com>
2021-06-11 23:57:28 +0530

    Added Custom Query Methods-Entity Design https://github.com/sowbharni-05/E-ComRest-Impl/issues/3#issue-910471175

--- src/main/java/com/banfico/EcomApplication/Dao/CustomerRepo.java
+++ src/main/java/com/banfico/EcomApplication/Dao/CustomerRepo.java
@@ -2,8 +2,6 @@ package com.banfico.EcomApplication.Dao;
 
 import com.banfico.EcomApplication.Model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
