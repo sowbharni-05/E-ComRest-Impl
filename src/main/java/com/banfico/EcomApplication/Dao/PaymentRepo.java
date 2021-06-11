46cc02a1492a9f7cc79e369f9ebb38e659830f5b
sowbharni-05 <85208599+sowbharni-05@users.noreply.github.com>
2021-06-12 01:24:26 +0530

    Added Custom Query in Payment Repo-Dao design https://github.com/sowbharni-05/E-ComRest-Impl/issues/6#issue-911158628
    Edited wrong Commit

--- src/main/java/com/banfico/EcomApplication/Dao/PaymentRepo.java
+++ src/main/java/com/banfico/EcomApplication/Dao/PaymentRepo.java
@@ -4,7 +4,6 @@ import com.banfico.EcomApplication.Model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
