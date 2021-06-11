0ce0e191fd59949cb28c2f8ad826baa3bcfc33a0
Sowbharnika <sowbharnika.karthikeyan@banfico.com>
2021-06-11 23:57:28 +0530

    Added Custom Query Methods-Dao Design https://github.com/sowbharni-05/E-ComRest-Impl/issues/6#issue-911158628

--- src/main/java/com/banfico/EcomApplication/Dao/OrderRepo.java
+++ src/main/java/com/banfico/EcomApplication/Dao/OrderRepo.java
@@ -4,6 +4,11 @@ import com.banfico.EcomApplication.Model.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepo extends JpaRepository<OrderDetail,Integer> {
+
+    List<OrderDetail> findByOrderByOrderDateAsc();
+
 }
