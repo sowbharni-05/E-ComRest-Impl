63db8e2068522b369d323046d6cc138d9ff74ada
sowbharni-05 <sowbharnika.karthikeyan@banfico.com>
2021-06-21 10:56:34 +0530

    Added SpringApplicationBuilder - Main Class Design https://github.com/sowbharni-05/E-ComRest-Impl/issues/2#issue-910201968

--- src/main/java/com/banfico/EcomApplication/EcomApplication.java
+++ src/main/java/com/banfico/EcomApplication/EcomApplication.java
@@ -2,12 +2,28 @@ package com.banfico.EcomApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
-public class EcomApplication {
+public class EcomApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(EcomApplication.class);
+	}
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.banfico.EcomApplication")).build();
+	}
 }
