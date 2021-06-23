394f355c73f97cec9dac8f9bf4e0d1051dd363bc
sowbharni-05 <sowbharnika.karthikeyan@banfico.com>
2021-06-23 16:49:10 +0530

    Added SwaggerAnnotations - https://github.com/sowbharni-05/E-ComRest-Impl/issues/10#issue-928144412

--- src/main/java/com/banfico/EcomApplication/controller/CustomerController.java
+++ src/main/java/com/banfico/EcomApplication/controller/CustomerController.java
@@ -2,10 +2,16 @@ package com.banfico.EcomApplication.controller;
 
 import com.banfico.EcomApplication.entity.*;
 import com.banfico.EcomApplication.entity.enumtype.OrderStatus;
+import com.banfico.EcomApplication.model.Customer;
+import com.banfico.EcomApplication.model.OrderDetail;
+import com.banfico.EcomApplication.model.Payment;
+import com.banfico.EcomApplication.model.Shipping;
 import com.banfico.EcomApplication.service.CustomerService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,54 +25,80 @@ public class CustomerController{
     @Autowired
     private CustomerService customerservice;
 
+    @ApiOperation(nickname = "GetCustomerInfo",value = "GetCustomerDetails",notes = "View Customer Details with its mapped Orders")
     @RequestMapping(path="/",method = RequestMethod.GET)
-    public ResponseEntity<List<CustomerEntity>> getCustomer(){
+    public ResponseEntity<List<Customer>> getCustomer(){
 
         return customerservice.getCustomerInfo();
     }
+
+    @ApiOperation(nickname = "AddCustomerInfo",value = "PostCustomerDetails",notes = "Post Customer Details")
     @RequestMapping(path = "/",method = RequestMethod.POST)
-    public ResponseEntity<HttpStatus> addCustomer(@RequestBody CustomerEntity customerEntity){
-        return customerservice.addCustomerInfo(customerEntity);
+    public ResponseEntity<HttpStatus> addCustomer(@Valid @RequestBody Customer customer){
+        return customerservice.addCustomerInfo(customer);
     }
+
+    @ApiOperation(nickname = "UpdateCustomerInfo",value = "UpdateCustomerDetails",notes = "Provide an id to update address,phone number of a specific Customer")
     @RequestMapping(path="/", method = RequestMethod.PATCH)
     public ResponseEntity<CustomerEntity> updateCustomer(@Valid @RequestParam int id, @RequestParam String address, @RequestParam String phno){
         return customerservice.updateCustomer(id,address,phno);
 
     }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Server error"),
+            @ApiResponse(code = 404, message = "Service not found"),
+            @ApiResponse(code = 400, message = "BadRequest")})
+    @ApiOperation(value = "DeleteCustomerById",notes = "Provide an id to delete an specific customer",
+            response =CustomerEntity.class,nickname = "DeleteCustomer")
     @RequestMapping(path="/{id}",method = RequestMethod.DELETE)
-    public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable int id){
+    public ResponseEntity<HttpStatus> deleteCustomer(@ApiParam(name = "CustomerId",defaultValue = "0") @PathVariable int id){
         return customerservice.deleteCustomerInfo(id);
     }
+
+    @ApiOperation(nickname = "AddOrderInfo",value = "PostOrderDetails",notes = "Post Order Details with its mapped Customer")
     @RequestMapping(path="/orders",method = RequestMethod.POST)
-    public ResponseEntity<HttpStatus> addOrder(@RequestBody OrderDetailEntity order)
+    public ResponseEntity<HttpStatus> addOrder(@RequestBody OrderDetail order)
     {
         return customerservice.addOrderInfo(order);
     }
+
+    @ApiOperation(value = "UpdateOrderStatus",nickname = "UpdateOrderInfo",
+            notes = "Provide an id to update OrderStatus of a specific Order details")
     @RequestMapping(path="/orders", method = RequestMethod.PUT)
     public ResponseEntity<OrderDetailEntity> updateStatus(@RequestParam(value = "id") int id, @RequestParam(value = "status") OrderStatus status){
         return customerservice.updateStatus(id,status);
     }
+
+    @ApiOperation(value = "DeleteOrderDetailsById",nickname = "DeleteOrderInfo",notes = "Provide an id to delete specific Order")
     @RequestMapping(path="/orders",method = RequestMethod.DELETE)
     public ResponseEntity<HttpStatus> deleteOrder(@PathParam(value = "id") int id){
         return customerservice.deleteOrderInfo(id);
     }
+
+    @ApiOperation(value = "GetOrderDetails",nickname = "GetOrderInfo",notes = "View order details")
     @RequestMapping(path="/orders",method = RequestMethod.GET)
-    public ResponseEntity<List<OrderDetailEntity>> getOrderdetail(){
+    public ResponseEntity<List<OrderDetail>> getOrderdetail(){
         return customerservice.getOrderDetail();
     }
+
+    @ApiOperation(value = "GetShippingDetails",nickname = "GetShippingInfo",notes = "View Shipping details with its Orders")
     @RequestMapping(path="/orders/shipping",method = RequestMethod.GET)
-    public ResponseEntity<List<ShippingEntity>> getShipping()
+    public ResponseEntity<List<Shipping>> getShipping()
     {
         return customerservice.getShippingInfo();
     }
 
+    @ApiOperation(value = "GetPaymentDetails",nickname = "GetPaymentInfo",notes = "View Order's Payment details")
     @RequestMapping(path="/orders/payment",method = RequestMethod.GET)
-    public ResponseEntity<List<PaymentEntity>> getPayment()
+    public ResponseEntity<List<Payment>> getPayment()
     {
         return customerservice.getPaymentInfo();
     }
+
+    @ApiOperation(value = "GetPaymentDetailsByPayType",nickname = "GetPaymentInfo",notes = "Provide an payment type to get specific Payment type details")
     @RequestMapping(path = "/orders/payment/{payType}",method=RequestMethod.GET)
-    public ResponseEntity<List<PaymentEntity>> getPaymentType(@PathParam("payType") String pType){
+    public ResponseEntity<List<Payment>> getPaymentType(@PathParam("payType") String pType){
         return customerservice.getPaymentTypeInfo(pType);
     }
 
--- src/main/java/com/banfico/EcomApplication/controller/ProductController.java
+++ src/main/java/com/banfico/EcomApplication/controller/ProductController.java
@@ -1,8 +1,8 @@
 package com.banfico.EcomApplication.controller;
-
-import com.banfico.EcomApplication.entity.CategoryEntity;
-import com.banfico.EcomApplication.entity.ProductEntity;
+import com.banfico.EcomApplication.model.Category;
+import com.banfico.EcomApplication.model.Product;
 import com.banfico.EcomApplication.service.ProductService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,39 +16,47 @@ public class ProductController {
     @Autowired
     private ProductService productservice;
 
+    @ApiOperation(value = "PostCategoryDetails",nickname = "AddCategory",notes="Post Category Details")
     @RequestMapping(path = "/",method = RequestMethod.POST)
-    public ResponseEntity<HttpStatus> addCategory(@RequestBody CategoryEntity categoryEntityInfo)
+    public ResponseEntity<HttpStatus> addCategory(@RequestBody Category category)
     {
-
-        return productservice.addCategoryInfo(categoryEntityInfo);
+        return productservice.addCategoryInfo(category);
     }
+
+    @ApiOperation(value = "GetCategoryDetails",nickname = "GetCategoryInfo",notes = "View Category Details with its mapped Products")
     @RequestMapping(path="/",method = RequestMethod.GET)
-    public ResponseEntity<List<CategoryEntity>> getCategory(){
+    public ResponseEntity<List<Category>> getCategory(){
 
         return productservice.getCategoryInfo();
     }
+
+    @ApiOperation(value = "GetCategoryByName",nickname = "GetCategoryInfo",notes = "Provide a category name to look up a specific Category details")
     @RequestMapping(path="/name",method = RequestMethod.GET)
-    public ResponseEntity<List<CategoryEntity>> getCategoryByname(@RequestParam(name = "name") String categoryName){
+    public ResponseEntity<List<Category>> getCategoryByname(@RequestParam(name = "name") String categoryName){
         return productservice.getCategoryByName(categoryName);
     }
 
-
+    @ApiOperation(value ="DeleteCategoryById",nickname = "DeleteCategoryInfo",notes = "Provide an id to delete a specific Category")
     @RequestMapping(path = "/{id}",method= RequestMethod.DELETE)
     public ResponseEntity<HttpStatus> deleteCategory(@PathVariable int id){
 
         return productservice.deleteCategoryInfo(id);
     }
-    @RequestMapping(path = "/products",method = RequestMethod.POST)
-    public ResponseEntity<HttpStatus> addProduct(@RequestBody ProductEntity productEntityInfo)
-    {
 
-        return productservice.addProductInfo(productEntityInfo);
+    @ApiOperation(value = "PostProductDetails",nickname = "AddProductInfo",notes = "Post Product with its mapped Category")
+    @RequestMapping(path = "/products",method = RequestMethod.POST)
+    public ResponseEntity<HttpStatus> addProduct(@RequestBody Product productInfo)
+    {
+        return productservice.addProductInfo(productInfo);
     }
+
+    @ApiOperation(value = "GetProductDetails",nickname = "GetProductInfo",notes = "View Product Details sorted ascending by price")
     @RequestMapping(path="/products",method = RequestMethod.GET)
-    public ResponseEntity<List<ProductEntity>> getProductByAsc(){
+    public ResponseEntity<List<Product>> getProductByAsc(){
         return productservice.getProductInfoSort();
     }
 
+    @ApiOperation(value = "DeleteProductDetailById",nickname = "DeleteProductInfo",notes = "Provide an id to delete a specific product details")
     @RequestMapping(path = "/products/{id}",method= RequestMethod.DELETE)
     public ResponseEntity<HttpStatus> deleteProduct(@PathVariable int id){
 
