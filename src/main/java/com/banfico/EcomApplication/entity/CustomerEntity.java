f753db53e74be4aac01507debdf5d0dba8a1c3d5
sowbharni-05 <sowbharnika.karthikeyan@banfico.com>
2021-07-02 19:15:12 +0530

    Added @ElementCollection & @EntityListeners Entity Design https://github.com/sowbharni-05/E-ComRest-Impl/issues/3#issue-910471175

--- src/main/java/com/banfico/EcomApplication/entity/CustomerEntity.java
+++ src/main/java/com/banfico/EcomApplication/entity/CustomerEntity.java
@@ -7,8 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 
 @AllArgsConstructor
@@ -18,15 +22,27 @@ import java.util.List;
 @Setter
 @Entity
 @Table(name="customer")
+@EntityListeners(AuditingEntityListener.class)
 public class CustomerEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int customerId;
     private String customerName;
     private String emailId;
-    private String address;
+  //  private String address;
     private String phNo;
-    private String pinCode;
+  // private String pinCode;
+
+    @CreatedDate
+    private Date CreatedDate;
+    @LastModifiedDate
+    private Date ModifiedDate;
+
+    @ElementCollection
+    @CollectionTable(name = "Customer_Address",joinColumns = @JoinColumn(name = "CustomerId",referencedColumnName = "customerId"))
+    private List<Address> addresses;
+   // private Set<Address> addressSet;ImmutableCollection error
+
 
     @JsonManagedReference("customerRef")
     @OneToMany(targetEntity = OrderDetailEntity.class,
